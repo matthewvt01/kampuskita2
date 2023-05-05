a181cdce09f72216e8feb33b1d0e85ca66b04175
matthewvt01 <matthewvincentius01@gmail.com>
2023-05-05 15:01:48 +0700

    Layout

---
+++ app/src/main/java/com/si6b/kampuskita2/Activity/TambahActivity.java
@@ -0,0 +1,16 @@
+package com.si6b.kampuskita2.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.si6b.kampuskita2.R;
+
+public class TambahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah);
+    }
+}
