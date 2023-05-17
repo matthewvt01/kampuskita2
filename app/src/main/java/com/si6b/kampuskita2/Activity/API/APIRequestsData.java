6a71b2b199e2aee59ab51b125a9ac065e0d34537
matthewvt01 <matthewvincentius01@gmail.com>
2023-05-17 12:59:56 +0700

    APIRequestsData Retrofit CRUD

--- app/src/main/java/com/si6b/kampuskita2/Activity/API/APIRequestsData.java
+++ app/src/main/java/com/si6b/kampuskita2/Activity/API/APIRequestsData.java
@@ -1,4 +1,37 @@
 package com.si6b.kampuskita2.Activity.API;
 
+import com.si6b.kampuskita2.Activity.Model.ModelResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 public interface APIRequestsData {
+    @GET("retrieve.php")
+    Call<ModelResponse> ardRetrieve();
+
+    @FormUrlEncoded
+    @POST("create.php")
+    Call<ModelResponse> ardCreate(
+            @Field("nama") String nama,
+            @Field("kota") String kota,
+            @Field("alamat") String alamat
+    );
+
+    @FormUrlEncoded
+    @POST("update.php")
+    Call<ModelResponse> ardUpdate(
+            @Field("id") String id,
+            @Field("nama") String nama,
+            @Field("kota") String kota,
+            @Field("alamat") String alamat
+    );
+
+    @FormUrlEncoded
+    @POST("delete.php")
+    Call<ModelResponse> ardDelete(
+            @Field("id") String id
+    );
 }
